42960f14ee96f4e4da5c700ecdf719113cea9fdf
ArrayRecord Team <array-record@google.com>
2023-11-13 11:06:45 -0800

    Adding examples of function usage
    PiperOrigin-RevId: 582022700

---
+++ beam/examples/example_gcs_conversion.py
@@ -0,0 +1,29 @@
+"""Execute this to convert an existing set of TFRecords to ArrayRecords."""
+
+
+from apache_beam.options import pipeline_options
+from array_record.beam.pipelines import convert_tf_to_arrayrecord_gcs
+
+## Set input and output patterns as specified
+input_pattern = 'gs://<YOUR_INPUT_BUCKET>/records/*.tfrecord'
+output_path = 'gs://<YOUR_OUTPUT_BUCKET>/records/'
+
+args = {'input': input_pattern, 'output': output_path}
+
+## Set pipeline options and uncomment in main() to run in Dataflow
+pipeline_options = pipeline_options.PipelineOptions(
+    runner='DataflowRunner',
+    project='<YOUR_PROJECT>',
+    region='<YOUR_REGION>',
+    requirements_file='requirements.txt'
+)
+
+
+def main():
+  convert_tf_to_arrayrecord_gcs(
+      args=args,
+      # pipeline_options=pipeline_options
+  ).run()
+
+if __name__ == '__main__':
+  main()
