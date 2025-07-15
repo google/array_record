97171bc144dc1722187f044e8396df5323da8ecc
Mateusz Sokół <mat646@gmail.com>
2025-07-15 08:49:47 +0000

    Add Windows build

--- oss/runner_common.sh
+++ oss/runner_common.sh
@@ -19,30 +19,33 @@ setup_env_vars_py() {
 # under $SOURCE_DIR/all_dist.
 build_and_test_array_record() {
   printf 'Creating ArrayRecord wheel for Python Version %s\n' "$PYTHON_VERSION"
-  if [ "$(uname)" = "Darwin" ]; then
-    setup_env_vars_py "$PYTHON_MAJOR_VERSION" "$PYTHON_MINOR_VERSION"
-    "$PYTHON_BIN" -m pip install -U setuptools wheel etils[epath]
-    sh "${SOURCE_DIR}"'/oss/build_whl.sh'
-  else
-    # Automatically decide which platform to build for by checking on which
-    # platform this runs.
-    AUDITWHEEL_PLATFORM='manylinux2014_'"$(uname -m)"
-    docker rmi -f array_record:${PYTHON_VERSION}
-    docker rm -f array_record
-    DOCKER_BUILDKIT=1 docker build --progress=plain --no-cache \
-      --build-arg AUDITWHEEL_PLATFORM="${AUDITWHEEL_PLATFORM}" \
-      --build-arg PYTHON_VERSION="${PYTHON_MAJOR_VERSION}""${PYTHON_MINOR_VERSION}" \
-      --build-arg BAZEL_VERSION="${BAZEL_VERSION}" \
-      -t array_record:"${PYTHON_VERSION}" "${SOURCE_DIR}"'/oss'
+  case "$(uname)" in
+    Darwin*|CYGWIN*|MINGW*|MSYS_NT*)
+      setup_env_vars_py "$PYTHON_MAJOR_VERSION" "$PYTHON_MINOR_VERSION"
+      "$PYTHON_BIN" -m pip install -U setuptools wheel etils[epath]
+      sh "${SOURCE_DIR}"'/oss/build_whl.sh'
+      ;;
+    *)
+      # Automatically decide which platform to build for by checking on which
+      # platform this runs.
+      AUDITWHEEL_PLATFORM='manylinux2014_'"$(uname -m)"
+      docker rmi -f array_record:${PYTHON_VERSION}
+      docker rm -f array_record
+      DOCKER_BUILDKIT=1 docker build --progress=plain --no-cache \
+        --build-arg AUDITWHEEL_PLATFORM="${AUDITWHEEL_PLATFORM}" \
+        --build-arg PYTHON_VERSION="${PYTHON_MAJOR_VERSION}""${PYTHON_MINOR_VERSION}" \
+        --build-arg BAZEL_VERSION="${BAZEL_VERSION}" \
+        -t array_record:"${PYTHON_VERSION}" "${SOURCE_DIR}"'/oss'
 
-    docker run --rm -a stdin -a stdout -a stderr \
-      --env PYTHON_VERSION="${PYTHON_MAJOR_VERSION}"'.'"${PYTHON_MINOR_VERSION}" \
-      --env PYTHON_MAJOR_VERSION="${PYTHON_MAJOR_VERSION}" \
-      --env PYTHON_MINOR_VERSION="${PYTHON_MINOR_VERSION}" \
-      --env BAZEL_VERSION="${BAZEL_VERSION}" \
-      --env AUDITWHEEL_PLATFORM="${AUDITWHEEL_PLATFORM}" \
-      -v "${SOURCE_DIR}":"${OUTPUT_DIR}" \
-      --name array_record array_record:"${PYTHON_VERSION}" \
-      sh oss/build_whl.sh
-  fi
+      docker run --rm -a stdin -a stdout -a stderr \
+        --env PYTHON_VERSION="${PYTHON_MAJOR_VERSION}"'.'"${PYTHON_MINOR_VERSION}" \
+        --env PYTHON_MAJOR_VERSION="${PYTHON_MAJOR_VERSION}" \
+        --env PYTHON_MINOR_VERSION="${PYTHON_MINOR_VERSION}" \
+        --env BAZEL_VERSION="${BAZEL_VERSION}" \
+        --env AUDITWHEEL_PLATFORM="${AUDITWHEEL_PLATFORM}" \
+        -v "${SOURCE_DIR}":"${OUTPUT_DIR}" \
+        --name array_record array_record:"${PYTHON_VERSION}" \
+        sh oss/build_whl.sh
+      ;;
+  esac
 }
