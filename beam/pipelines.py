2f47e5e309c3036a1278615ab93feb34c13ed6fe
Mateusz Sokół <mat646@gmail.com>
2025-07-30 11:37:23 +0000

    Add Text inputs support for Beam module

--- beam/pipelines.py
+++ beam/pipelines.py
@@ -1,5 +1,7 @@
 """Various opinionated Beam pipelines for testing different functionality."""
 
+from typing import Any, Callable
+
 import apache_beam as beam
 from apache_beam.coders import coders
 from . import arrayrecordio
@@ -17,7 +19,8 @@ def example_to_tfrecord(
     num_shards=1,
     args=def_args,
     pipeline_options=def_pipeline_options):
-  """Beam pipeline for creating example TFRecord data.
+  """
+  Beam pipeline for creating example TFRecord data.
 
   Args:
     num_shards: Number of files
@@ -31,13 +34,13 @@ def example_to_tfrecord(
   p1 = beam.Pipeline(options=pipeline_options)
   _ = (
       p1
-      | 'Create' >> beam.Create(example.generate_movie_examples())
-      | 'Write'
+      | "Create" >> beam.Create(example.generate_movie_examples())
+      | "Write"
       >> beam.io.WriteToTFRecord(
-          args['output'],
+          args["output"],
           coder=coders.ToBytesCoder(),
           num_shards=num_shards,
-          file_name_suffix='.tfrecord',
+          file_name_suffix=".tfrecord",
       )
   )
   return p1
@@ -60,22 +63,41 @@ def example_to_arrayrecord(
   p1 = beam.Pipeline(options=pipeline_options)
   _ = (
       p1
-      | 'Create' >> beam.Create(example.generate_movie_examples())
-      | 'Write'
+      | "Create" >> beam.Create(example.generate_movie_examples())
+      | "Write"
       >> arrayrecordio.WriteToArrayRecord(
-          args['output'],
+          args["output"],
           coder=coders.ToBytesCoder(),
           num_shards=num_shards,
-          file_name_suffix='.arrayrecord',
+          file_name_suffix=".arrayrecord",
+      )
+  )
+  return p1
+
+
+def _convert_to_array_record_disk(
+    num_shards, args, pipeline_options, file_type, beam_fn
+):
+  p1 = beam.Pipeline(options=pipeline_options)
+  _ = (
+      p1
+      | f"Read {file_type}" >> beam_fn(args["input"])
+      | "Write ArrayRecord"
+      >> arrayrecordio.WriteToArrayRecord(
+          args["output"],
+          coder=coders.ToBytesCoder(),
+          num_shards=num_shards,
+          file_name_suffix=".arrayrecord",
       )
   )
   return p1
 
 
 def convert_tf_to_arrayrecord_disk(
-    num_shards=1, args=def_args, pipeline_options=def_pipeline_options
+    *, num_shards=1, args=def_args, pipeline_options=def_pipeline_options
 ):
-  """Convert TFRecords to ArrayRecords using sink/sharding functionality.
+  """
+  Convert TFRecords to ArrayRecords using sink/sharding functionality.
 
   THIS ONLY WORKS FOR DISK ARRAYRECORD WRITES
 
@@ -88,25 +110,66 @@ def convert_tf_to_arrayrecord_disk(
     Beam Pipeline object
   """
 
+  return _convert_to_array_record_disk(
+    num_shards, args, pipeline_options, "TFRecord", beam.io.ReadFromTFRecord
+  )
+
+
+def convert_text_to_array_record_disk(
+    *, num_shards=1, args=def_args, pipeline_options=def_pipeline_options
+):
+  """
+  Convert Text files to ArrayRecords using sink/sharding functionality.
+
+  THIS ONLY WORKS FOR DISK ARRAYRECORD WRITES
+
+  Args:
+    num_shards: Number of files
+    args: Custom arguments
+    pipeline_options: Beam arguments in dict format
+
+  Returns:
+    Beam Pipeline object
+  """
+
+  return _convert_to_array_record_disk(
+    num_shards, args, pipeline_options, "Text file", beam.io.ReadFromText
+  )
+
+
+def _convert_to_arrayrecord_disk_match_shards(args, pipeline_options, beam_fn):
   p1 = beam.Pipeline(options=pipeline_options)
-  _ = (
+  initial = (
       p1
-      | 'Read TFRecord' >> beam.io.ReadFromTFRecord(args['input'])
-      | 'Write ArrayRecord'
+      | "Start" >> beam.Create([args["input"]])
+      | "Read" >> beam_fn(with_filename=True)
+  )
+
+  file_count = (
+      initial
+      | "Group" >> beam.GroupByKey()
+      | "Count Shards" >> beam.combiners.Count.Globally()
+  )
+
+  _ = (
+      initial
+      | "Drop Filename" >> beam.Map(lambda x: x[1])
+      | "Write ArrayRecord"
       >> arrayrecordio.WriteToArrayRecord(
-          args['output'],
+          args["output"],
           coder=coders.ToBytesCoder(),
-          num_shards=num_shards,
-          file_name_suffix='.arrayrecord',
+          num_shards=beam.pvalue.AsSingleton(file_count),
+          file_name_suffix=".arrayrecord",
       )
   )
   return p1
 
 
 def convert_tf_to_arrayrecord_disk_match_shards(
-    args=def_args, pipeline_options=def_pipeline_options
+    *, args=def_args, pipeline_options=def_pipeline_options
 ):
-  """Convert TFRecords to matching number of ArrayRecords.
+  """
+  Convert TFRecords to matching number of ArrayRecords.
 
   THIS ONLY WORKS FOR DISK ARRAYRECORD WRITES
 
@@ -118,40 +181,62 @@ def convert_tf_to_arrayrecord_disk_match_shards(
     Beam Pipeline object
   """
 
+  return _convert_to_arrayrecord_disk_match_shards(
+    args, pipeline_options, beam.io.ReadAllFromTFRecord
+  )
+
+
+def convert_text_to_arrayrecord_disk_match_shards(
+    *, args=def_args, pipeline_options=def_pipeline_options
+):
+  """
+  Convert Text files to matching number of ArrayRecords.
+
+  THIS ONLY WORKS FOR DISK ARRAYRECORD WRITES
+
+  Args:
+    args: Custom arguments
+    pipeline_options: Beam arguments in dict format
+
+  Returns:
+    Beam Pipeline object
+  """
+
+  return _convert_to_arrayrecord_disk_match_shards(
+    args, pipeline_options, beam.io.ReadAllFromText
+  )
+
+
+def _convert_to_arrayrecord_gcs(
+    overwrite_extension, file_path_suffix, args, pipeline_options, beam_fn
+):
   p1 = beam.Pipeline(options=pipeline_options)
-  initial = (
-      p1
-      | 'Start' >> beam.Create([args['input']])
-      | 'Read' >> beam.io.ReadAllFromTFRecord(with_filename=True)
-  )
-
-  file_count = (
-      initial
-      | 'Group' >> beam.GroupByKey()
-      | 'Count Shards' >> beam.combiners.Count.Globally()
-  )
-
   _ = (
-      initial
-      | 'Drop Filename' >> beam.Map(lambda x: x[1])
-      | 'Write ArrayRecord'
-      >> arrayrecordio.WriteToArrayRecord(
-          args['output'],
-          coder=coders.ToBytesCoder(),
-          num_shards=beam.pvalue.AsSingleton(file_count),
-          file_name_suffix='.arrayrecord',
+      p1
+      | "Start" >> beam.Create([args["input"]])
+      | "Read" >> beam_fn(with_filename=True)
+      | "Group" >> beam.GroupByKey()
+      | "Write to ArrayRecord in GCS"
+      >> beam.ParDo(
+          dofns.ConvertToArrayRecordGCS(),
+          args["output"],
+          file_path_suffix=file_path_suffix,
+          overwrite_extension=overwrite_extension,
       )
   )
   return p1
 
 
 def convert_tf_to_arrayrecord_gcs(
+    *,
     overwrite_extension=False,
-    file_path_suffix='.arrayrecord',
+    file_path_suffix=".arrayrecord",
     args=def_args,
-    pipeline_options=def_pipeline_options):
-  """Convert TFRecords to ArrayRecords in GCS 1:1.
-  
+    pipeline_options=def_pipeline_options
+):
+  """
+  Convert TFRecords to ArrayRecords in GCS 1:1.
+
   Args:
     overwrite_extension: Boolean making DoFn attempt to overwrite extension
     file_path_suffix: Intended suffix for overwrite or append
@@ -162,18 +247,31 @@ def convert_tf_to_arrayrecord_gcs(
     Beam Pipeline object
   """
 
-  p1 = beam.Pipeline(options=pipeline_options)
-  _ = (
-      p1
-      | 'Start' >> beam.Create([args['input']])
-      | 'Read' >> beam.io.ReadAllFromTFRecord(with_filename=True)
-      | 'Group' >> beam.GroupByKey()
-      | 'Write to ArrayRecord in GCS'
-      >> beam.ParDo(
-          dofns.ConvertToArrayRecordGCS(),
-          args['output'],
-          file_path_suffix=file_path_suffix,
-          overwrite_extension=overwrite_extension,
-      )
+  return _convert_to_arrayrecord_gcs(
+    overwrite_extension, file_path_suffix, args, pipeline_options, beam.io.ReadAllFromTFRecord
   )
-  return p1
+
+
+def convert_text_to_arrayrecord_gcs(
+    *,
+    overwrite_extension=False,
+    file_path_suffix=".arrayrecord",
+    args=def_args,
+    pipeline_options=def_pipeline_options
+):
+  """
+  Convert Text files to ArrayRecords in GCS 1:1.
+
+  Args:
+    overwrite_extension: Boolean making DoFn attempt to overwrite extension
+    file_path_suffix: Intended suffix for overwrite or append
+    args: Custom arguments
+    pipeline_options: Beam arguments in dict format
+
+  Returns:
+    Beam Pipeline object
+  """
+
+  return _convert_to_arrayrecord_gcs(
+    overwrite_extension, file_path_suffix, args, pipeline_options, beam.io.ReadAllFromText
+  )
