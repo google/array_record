6db8c5fcf051fb9a06454b97b62292561bf074fe
Mateusz Sokol <mat646@gmail.com>
2025-08-12 07:31:07 -0700

    Upgrade protobuf to match with TF 2.20.0rc0
    PiperOrigin-RevId: 794102749

--- oss/build_whl.sh
+++ oss/build_whl.sh
@@ -87,12 +87,12 @@ function main() {
   echo $(date) : "=== Output wheel file is in: ${DEST}"
 
   # Install ArrayRecord from the wheel and run smoke tests.
-  $PYTHON_BIN -m pip install --find-links="${DEST}" --pre array-record
+  $PYTHON_BIN -m pip install ${OUTPUT_DIR}/all_dist/array_record*.whl
   $PYTHON_BIN -c 'import array_record'
   $PYTHON_BIN -c 'from array_record.python import array_record_data_source'
   # TF is not available on Python 3.13 and above.
   if [ "$(uname)" != "Darwin" ] && (( "${PYTHON_MINOR_VERSION}" < 13 )); then
-    $PYTHON_BIN -m pip install jax tensorflow grain
+    $PYTHON_BIN -m pip install jax tensorflow>=2.20.0rc0 grain
     $PYTHON_BIN oss/test_import_grain.py
     $PYTHON_BIN oss/test_import_tensorflow.py
   fi
